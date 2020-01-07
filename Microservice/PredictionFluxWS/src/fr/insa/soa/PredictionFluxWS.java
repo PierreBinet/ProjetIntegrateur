6153c4179f9863718cac91b7c17d5a82f9cf2e39
ConstanceGay <constancegaycsi@gmail.com>
2020-01-07 17:00:29 +0100

    updated libs & prediction

--- Microservice/PredictionFluxWS/src/fr/insa/soa/PredictionFluxWS.java
+++ Microservice/PredictionFluxWS/src/fr/insa/soa/PredictionFluxWS.java
@@ -1,8 +1,17 @@
 package fr.insa.soa;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("prediction")
 public class PredictionFluxWS {
-
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getLongueur(){
+		return "Hey!";
+	}
+	
 }
