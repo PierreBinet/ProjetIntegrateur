3dc7104a66dba211c6081260b1cbd1a6e0e5aacd
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2020-01-19 09:48:24 +0100

    add all trip attributes

--- BDD/hbase_trips/HBaseGetData.java
+++ BDD/hbase_trips/HBaseGetData.java
@@ -15,75 +15,74 @@ public class HBaseGetData {
 	private int month;
 	private int station;
 	private FileWriter fw;
-	
+
 	public HBaseGetData(int month, int station) {
 		this.month = month;
 		this.station = station;
-		
+
 		try {
 			this.fw = new FileWriter("trips.csv");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
-		
+
 	public void createCsv() {
-		
+
 		try {
-	      // Instantiating configuration class
-	      Configuration config = HBaseConfiguration.create();
-	      
-	      // Instantiating HTable class
-	      HTable table = new HTable(config, "trips");
-	
-	      // Instantiating the Scan class
-	      Scan scan = new Scan();
-/*
+			// Instantiating configuration class
+			Configuration config = HBaseConfiguration.create();
+
+			// Instantiating HTable class
+			HTable table = new HTable(config, "trips");
+
+			// Instantiating the Scan class
+			Scan scan = new Scan();
+			/*
  		// filter
- 		 * 
+			 
  		BinaryComparator binlimite = new BinaryComparator(rawlimite);
  		SingleColumnValueFilter filtre = new SingleColumnValueFilter(rawfam, rawcol, CompareOp.GREATER_OR_EQUAL, binlimite);
-		
+
 		SingleColumnValueFilter filtre1 = ;
 		QualifierFilter filtre2 = ;
 		FilterList conjonction = new FilterList(FilterList.Operator.MUST_PASS_ALL, filtre1, filtre2);
 		scan.setFilter(conjonction); */
-	      
-	      // Get the scan result
-	      ResultScanner scanner = table.getScanner(scan);
-	
-	      // write labels in csv file
-	      this.fw.write("\"tripduration\",\"start_station_id\",\"end_station_id\",\"bikeid\",\"usertype\",\"birth_year\",\"gender\",\"start_day\",\"start_wday\",\"start_month\",\"start_year\",\"start_hour\",\"start_minute\",\"start_second\",\"end_day\",\"end_wday\",\"end_month\",\"end_year\",\"end_hour\",\"end_minute\",\"end_second\"\n");
-	      
-	      int count = 0;
-	      
-	      // Read values from scan result
-	      for (Result result : scanner) {
+
+			// Get the scan result
+			ResultScanner scanner = table.getScanner(scan);
+
+			// write labels in csv file
+			this.fw.write("\"tripduration\",\"start_station_id\",\"end_station_id\",\"bikeid\",\"usertype\",\"birth_year\",\"gender\",\"start_day\",\"start_wday\",\"start_month\",\"start_year\",\"start_hour\",\"start_minute\",\"start_second\",\"end_day\",\"end_wday\",\"end_month\",\"end_year\",\"end_hour\",\"end_minute\",\"end_second\"\n");
+
+			int count = 0;
+
+			// Read values from scan result
+			for (Result result : scanner) {
 				Trip trip = new Trip(result);
 				trip.writeTripInFile(this.fw);
-				
+
 				count += 1;
 				if (count==10) {
 					break;
 				}
-	      }
-	      
-	      this.fw.close();
-	      
-	      //close the scanner
-    	  scanner.close();
-				
-	      } catch (IOException e) {
-	    	  e.printStackTrace();
-	      }
-	      
+			}
+
+			this.fw.close();
+
+			//close the scanner
+			scanner.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	
+
+	}
+
 	public static void main(String[] args) throws IOException {
 		HBaseGetData hb = new HBaseGetData(11, 1);
 		hb.createCsv();
 	}
 
-	
+
 }
--- BDD/hbase_trips/Trip.java
+++ BDD/hbase_trips/Trip.java
@@ -3,12 +3,10 @@ package hbase_trips;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.*;
 
-/* row : "tripduration","start_station_id","end_station_id","bikeid",
+/* trip row : "tripduration","start_station_id","end_station_id","bikeid",
  * "usertype","birth_year","gender",
  * "start_day","start_wday","start_month","start_year","start_hour","start_minute","start_second",
  * "end_day","end_wday","end_month","end_year","end_hour","end_minute","end_second",
@@ -20,20 +18,48 @@ public class Trip {
 	protected String start_station_id;
 	protected String end_station_id;
 	protected String bikeid;
-	protected static byte[] rawfam = Bytes.toBytes("info");
+	protected String usertype;
+	protected String birth_year;
+	protected String gender;
+	protected String start_day;
+	protected String start_wday;
+	protected String start_month;
+	protected String start_year;
+	protected String start_hour;
+	protected String start_minute;
+	protected String start_second;
+	protected String end_day;
+	protected String end_wday;
+	protected String end_month;
+	protected String end_year;
+	protected String end_hour;
+	protected String end_minute;
+	protected String end_second;
+	
+	protected byte[] rawfam = Bytes.toBytes("info");
 	
 	public Trip(Result result) {
 		this.tripduration = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("tripduration")));
 		this.start_station_id = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_station_id")));
 		this.end_station_id = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_station_id")));
 		this.bikeid = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("bikeid")));
-	}
-	
-	public Trip(String tripduration, String start_station_id, String end_station_id, String bikeid) {
-		this.tripduration = tripduration;
-		this.start_station_id = start_station_id;
-		this.end_station_id = end_station_id;
-		this.bikeid = bikeid;
+		this.usertype = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("usertype")));
+		this.birth_year = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("birth_year")));
+		this.gender = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("gender")));
+		this.start_day = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_day")));
+		this.start_wday = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_wday")));
+		this.start_month = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_month")));
+		this.start_year = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_year")));
+		this.start_hour = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_hour")));
+		this.start_minute = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_minute")));
+		this.start_second = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("start_second")));
+		this.end_day = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_day")));
+		this.end_wday = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_wday")));
+		this.end_month = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_month")));
+		this.end_year = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_year")));
+		this.end_hour = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_hour")));
+		this.end_minute = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_minute")));
+		this.end_second = Bytes.toString(result.getValue(this.rawfam, Bytes.toBytes("end_second")));
 	}
 	
 	public void writeTripInFile(FileWriter fw) {
