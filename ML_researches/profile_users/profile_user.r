81f65587be4e5f92902e27adfb620e8b9a295516
LucileH <lucile.hernandez23@gmail.com>
2020-01-28 21:58:59 +0100

    clustering profile_users R file updated

--- ML_researches/profile_users/profile_user.r
+++ ML_researches/profile_users/profile_user.r
@@ -1,23 +1,64 @@
 
 #########################
 #Fichier de preparation des donnees a du clustering dans le but de prédire le profil utilisateur.
-#author : Lucile
 #date : 03/12/2019
+#status : fonction get_borough_index trop lourde, améliorer ou bien inclure dans le preprocess. 
 #########################
 
 
+library(ggplot2)
+library(sf)
+library(plyr)
+library(dplyr)
 library(tidyverse)
+library(maps)
+library(tibble)
+library(purrr)
+
 
 travel_data <- read.csv("data/201306-citibike-tripdata.csv", encoding = "UTF-8")
+borough_data <- read.csv("data/nyc_zones.csv", encoding = "UTF-8")
+
+
+
+# traitement des valeurs vides
+travel_data <- na.omit(travel_data)
+travel_data <- travel_data %>% filter(birth.year != "NULL" & end.station.latitude != "NULL" & end.station.longitude !="NULL")
+
+#conversion des coordonnées des stations d'arrivée 
+as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
+travel_data$end.station.longitude <- mapply(as.numeric.factor, travel_data$end.station.longitude)
+travel_data$end.station.latitude <- mapply(as.numeric.factor, travel_data$end.station.latitude)
+
+#ajout du quartier à travel data
+borough_data$the_geom <- st_as_sfc(borough_data$the_geom)
+
+#pt1 = st_point(c(travel_data$start.station.longitude[1], travel_data$start.station.latitude[1]))
+
+get_borough_index <- function (long, lat){
+   index=NULL
+   pt1 = st_point(c(long,lat))
+   test = st_intersects(borough_data$the_geom,pt1)
+   for(i in 1:nrow(borough_data)) {
+     if (!(is.na(test[i]==0))){
+       index = i
+     }
+     else {i = i+1 }
+   }
+   return(index)
+}
+
+travel_data$borough_start <- mapply(get_borough_index, travel_data$start.station.longitude, travel_data$start.station.latitude)
+travel_data$borough_end <- mapply(get_borough_index, travel_data$end.station.longitude, travel_data$end.station.latitude)
+
+#plot(st_geometry(borough_data$the_geom))
 
 #nettoyage donnees travel
 travel_data <- subset(travel_data, select=c(tripduration,
                                             starttime, 
                                             stoptime,
-                                            start.station.latitude, 
-                                            start.station.longitude, 
-                                            end.station.latitude, 
-                                            end.station.longitude,
+                                            borough_start,
+                                            borough_end,
                                             usertype,
                                             birth.year,
                                             gender))
@@ -57,22 +98,29 @@ travel_data <- travel_data %>% mutate(stop_weekday = case_when(stop_weekday_lit 
 # ajouter un booléen is_weekend (si jour_semaine = 6 ou 7) ?
 travel_data <- travel_data %>% mutate(is_weekend = if_else(start_weekday >=6, 1, 0))
 
-# comment lier latitude et longitude ? (idea : clustering --> convert latitude + longitude in a unique geographical zone id)
-
 # sélection des colonnes
 
-dataset <- subset(travel_data, select=-c(tripduration,
+travel_data <- subset(travel_data, select=-c(tripduration,
                                          starttime,
                                          stoptime,
+                                         Start_date,
+                                         Stop_date,
                                          start_weekday_lit,
                                          stop_weekday_lit))
 
-# traitement des valeurs vides
-dataset <- na.omit(dataset)
-dataset <- dataset %>% filter(birth.year != "NULL")
 
-#ET POURQUOI PAS ? transformer l'année de naissance en tranche d'âge ? 
 
+
+
+#replace birth year into decade
+get_decade <- function(year){
+   decade = year - year %% 10
+   decade
+}
+
+travel_data$birth_year_decade <- as.numeric(as.character(travel_data$birth.year)) - as.numeric(as.character(travel_data$birth.year)) %% 10 
+travel_data <- subset(travel_data, select=-c(birth.year))
 #enregistrement du dataset .csv des entrees-sorties des velos en fct de la station et de l'heure
-write.csv(dataset, "data/profile_user_dataset.csv", row.names = FALSE)
+write.csv(travel_data, "data/clustering_profiling_dataset.csv", row.names = FALSE)
+
 
